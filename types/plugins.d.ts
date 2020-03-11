35401f976484a5e3295d6bfdc523fc52aece3d5a
gwillz <me@gwilyn.com>
2020-03-12 01:28:43 +1030

    Use official metalsmith typings, remove plugin typings, update postcss.

--- types/plugins.d.ts
+++ types/plugins.d.ts
@@ -1,60 +1,7 @@
 
-declare module "@goodthnx/metalsmith-webpack" {
-    import webpack from 'webpack'
-    
-    type Plugin = (...args: any[]) => void;
-    
-    type Config = Partial<Omit<webpack.Configuration, "entry" | "output">>;
-    
-    interface Options {
-        pattern?: string;
-        config?: true | string | Config | null;
-    }
-    
-    function plugin(options?: Options): Plugin;
-    export = plugin;
-}
-
-declare module "@goodthnx/metalsmith-postcss" {
-    import postcss from 'postcss'
-    
-    type Plugin = (...args: any[]) => void;
-    
-    type Config = Partial<postcss.ProcessOptions>;
-    
-    interface Options {
-        pattern?: string;
-        config?: true | string | Config | null;
-    }
-    
-    function plugin(options?: Options): Plugin;
-    export = plugin;
-}
-
-declare module "@goodthnx/metalsmith-handlebars" {
-    import handlebars from 'handlebars'
-    
-    type Plugin = (...args: any[]) => void;
-    
-    type Config = Partial<handlebars.RuntimeOptions>;
-    
-    interface Options {
-        pattern?: string;
-        extension?: string;
-        partials?: string | null;
-        helpers?: string | null;
-        layouts?: string | null;
-        config?: Config | null;
-    }
-    
-    function plugin(options?: Options): Plugin;
-    export = plugin;
-}
-
 declare module "metalsmith-watch" {
-    import handlebars from 'handlebars'
     
-    type Plugin = (...args: any[]) => void;
+    import type { Plugin } from 'metalsmith';
     
     function plugin(options?: any): Plugin;
     export = plugin;
